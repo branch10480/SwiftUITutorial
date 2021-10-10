f5ff5ec866b45eec884916d07c609737dd481a86
branch10480 <toshiharu.imaeda@gmail.com>
2021-10-10 13:53:36 +0900

    Creating and Combining Views

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    VStack {
+      MapView()
+        .frame(height: 300)
+        .ignoresSafeArea(edges: .top)
+      
+      CircleImage()
+        .offset(y: -130)
+        .padding(.bottom, -130)
+      
+      VStack(alignment: .leading) {
+        Text("Turtle Rock")
+          .font(.title)
+          .foregroundColor(.black)
+        
+        HStack {
+          Text("Joshua Tree National Park")
+          Spacer()
+          Text("California")
+        }
+        .font(.subheadline)
+        .foregroundColor(.secondary)
+      }
+      .padding()
+      
+      Divider()
+      
+      Text("About Turtle Rock")
+      
+      Text("Descriptive text goes here.")
+      
+      Spacer()
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
